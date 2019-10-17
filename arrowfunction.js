20a6097cb1e8cb671e4b8b687236774e044f7b0b
Ambeshgaurav <lpuambeshgaurav@gmail.com>
2019-10-17 18:21:58 +0530

    sol

---
+++ arrowfunction.js
@@ -0,0 +1,12 @@
+// const arr=[10,20,30,40,50];
+// let abc=arr.map((e1,index)=>{
+//     console.log("hello arrow function");
+//     console.log(arr);
+//      console.log(index);
+//      });
+let abc=()=>
+{
+    console.log("hello every one");
+    
+}
+abc();
